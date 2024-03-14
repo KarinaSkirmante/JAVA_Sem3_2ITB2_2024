1a07f54102ed972611a63143deab979e2a5ae013
KarinaSkirmante <karina.krinkele@gmail.com>
2024-03-14 14:03:31 +0200

    New: Post class

--- src/model/Post.java
+++ src/model/Post.java
@@ -1,5 +1,67 @@
 package model;
 
-public class Post {
+import java.time.LocalDateTime;
 
+public class Post {
+	//1.variables
+	private String title;
+	private String msg;
+	private LocalDateTime dateTime = LocalDateTime.now();
+	//TODO pārtaisīt uz User sarakstu
+	private int countOfLikes = 0;
+	
+	//2.get and set
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		if(title != null && title.length() > 5 && title.length() < 50)
+			this.title = title;
+		else 
+			this.title = "------";
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		if(msg != null && msg.length() > 5 && msg.length() < 1000)
+			this.msg = msg;
+		else
+			this.msg = "---------";
+	}
+	public int getCountOfLikes() {
+		return countOfLikes;
+	}
+	
+	//TODO iespējams mainīsies, ja izveidosim User sarakstu, kuri spiež Like iezīmes
+	public void incrementCountOfLikes() {
+		this.countOfLikes++;
+	}
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+	
+	//3.constructors
+	
+	public Post()
+	{
+		setTitle("Test title");
+		setMsg("Test description");
+		
+	}
+	
+	public Post(String title, String msg) {
+		setTitle(title);
+		setMsg(msg);
+	}
+	//4.toString
+	
+	public String toString() {
+		 return title + " (" + dateTime + ") \n" + msg + "\n[ " + countOfLikes + "]\n";
+	}
+	
+	
+	//5.other functions
+	
+	
 }
